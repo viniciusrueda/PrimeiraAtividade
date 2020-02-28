6745a13dfc7bb0f49fa9505aae2e12e1731fb301
viniciusrueda <viniciusemail9@gmail.com>
2020-02-27 23:30:41 -0300

    Versão Final.

---
+++ app/src/main/java/com/projetoapp/primeiraatividade/MainActivity.java
@@ -0,0 +1,15 @@
+package com.projetoapp.primeiraatividade;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Versão final", Toast.LENGTH_SHORT).show();
+    }
+}
